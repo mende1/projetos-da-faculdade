b482b0841fed54e081fd84a788b54f1fb3512e26
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-03-27 19:49:42 -0300

    header default files competitive

--- competitive/default_files/main.cpp
+++ competitive/default_files/main.cpp
@@ -1,40 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+#define int ll
+#define fori(n) for(int i=0; i<int(n); i++)
+
+#define cins(s) string s; cin>>s;
+#define cini(i) int i; cin>>i;
+#define cind(d) double d; cin>>d;
+#define cinai(a, n) vi a(n); fori(n) { cin>>a[i]; }
+#define cinas(s, n) vs s(n); fori(n) { cin>>s[i]; }
+#define cinad(a, n) vd a(n); fori(n) { cin>>a[i]; }
+
+typedef vector<double> vd;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<string> vs;
+
+
 void solve() {
 
-	int n;
-	cin >> n;
-
-	int a[n], sp[n+1];
-	
-	for (int i=0; i<n; i++) {
-		cin >> a[i];
-		sp[i+1] = 0;
-	}
-
-	for (int i=0; i<n; i++) {
-		int sum = a[i];
-
-		for (int j=i+1; j<n; j++) {
-
-			sum += a[j];
-
-			if (sum > n) break;
-
-			sp[sum] = 1;
-		}
-
-	}
-
-	int special = 0;
-
-	for (int i=0; i<n; i++) {
-
-		if (sp[a[i]]) special++;
-	}
-
-	cout << special << endl;
+	return;
 }
 
 int main(){
