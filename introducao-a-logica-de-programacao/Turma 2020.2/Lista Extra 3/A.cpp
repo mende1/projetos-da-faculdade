848a2ac581f622785bfd747eb9a2af7dd7aab888
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-06-02 19:41:01 -0300

    arq.comp: projeto 4 init

--- introducao-a-logica-de-programacao/Turma 2020.2/Lista Extra 3/A.cpp
+++ introducao-a-logica-de-programacao/Turma 2020.2/Lista Extra 3/A.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	
+	int n;
+	cin >> n;
 
+	string arr[n];
+
+	for (int i=0; i<n; i++) {
+		getline(cin, arr[i]);
+	}
+
+	int q;
+	cin >> q;
+
+	string req[q];
+
+	for (int i=0; i<q; i++) {
+		getline(cin, req[i]);
+	}
+
+	for (int i=0; i<q; i++) {
+		string key = req[i];
+
+		int m;
+		int left = 0;
+		int right = n-1;
+
+		while (left <= right) {
+
+			m = (left + right) / 2;
+
+			if (key == arr[m]) {
+				break;
+			}
+			else if (key < arr[m]) {
+				right = m - 1;
+			}
+			else {
+				left = m + 1;
+			}
+		}
+
+		cout << arr[m];
+
+		if (key == arr[m]) {
+			cout << " foi extinto :(\n";
+		}
+		else {
+			cout << " vive!\n";
+		}
+	}
 }
