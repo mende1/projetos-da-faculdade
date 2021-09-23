eda88ded32dea59e40045ee465a4404ca49ec185
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-09-22 22:16:08 -0300

    monitoria ilp: lista 1

---
+++ monitoria-ilp/lista1/A.py
@@ -0,0 +1,4 @@
+espaco, tempo = input().split()
+espaco = int(espaco)
+tempo = int(tempo)
+print(espaco // tempo)
---
+++ monitoria-ilp/lista1/B.py
@@ -0,0 +1,4 @@
+a, b, c, d, e = [int(it) for it in input().split()]
+
+total = a - (b + c + d + e)
+print(total)
