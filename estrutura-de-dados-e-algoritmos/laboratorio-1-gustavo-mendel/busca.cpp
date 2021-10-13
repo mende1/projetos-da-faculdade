282df46df427714e108883921ab04596db645d22
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-10-13 10:55:14 -0300

    eda: laboratorio 1

--- estrutura-de-dados-e-algoritmos/laboratorio-1-gustavo-mendel/busca.cpp
+++ estrutura-de-dados-e-algoritmos/laboratorio-1-gustavo-mendel/busca.cpp
@@ -6,9 +6,41 @@
 
 using namespace std;
 
-#define MAX 20
+#define MAX 100
+#define TESTES 100
 
-bool buscaSeq(vector<int> v, int k) {
+bool buscaSeq(vector<int> v, int k, int * cont) {
+
+    for (int i=0; i<v.size(); i++) {
+        *cont += 1;
+        if (k == v[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool buscaBin (vector<int> v, int k, int * cont) {
+
+    sort(v.begin(), v.end());
+
+    int esq = 0, dir = v.size() - 1;
+
+    while (esq <= dir) {
+        int meio = (esq + dir) / 2;
+        *cont += 1;
+
+        if (v[meio] == k) {
+            return true;
+        }
+        else if (v[meio] > k) {
+            dir = meio - 1;
+        }
+        else {
+            esq = meio + 1;
+        }
+    }
 
     return false;
 }
@@ -36,10 +68,29 @@ int main() {
     srand ( time(NULL) );
 
     for (unsigned int i=0; i < MAX; i++) {
-        v.push_back(rand() % MAX*10);
+        v.push_back(rand() % MAX);
     }
 
     print_vet(v);
+
+    for (unsigned int i=0; i < TESTES; i++) {
+        int k = rand() % MAX;
+
+        // Busca Sequencial
+
+        int cont = 0, *p_cont = &cont;
+        bool achou = buscaSeq(v, k, p_cont);
+        string res = (achou) ? "SIM" :  "NAO";
+        cout << "Busc. Seq. ->\t Chave: " << k << ";\tAchou? " << res << ";\tQntd Ops: " << cont << endl;
+
+        // Busca Binária
+
+        int cont2 = 0, *p_cont2 = &cont2;
+        achou = buscaBin(v, k, p_cont2);
+        res = (achou) ? "SIM" :  "NAO";
+        cout << "Busc. Bin. ->\t Chave: " << k << ";\tAchou? " << res << ";\tQntd Ops: " << cont2 << endl << endl;
+
+    }
     	
  	return 0;        
 }
